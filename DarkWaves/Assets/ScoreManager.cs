ecff4b8a1f65e30bb061cfaca3b7187ab26e1529
Alberto Armijo Ruiz <armijoalb@correo.ugr.es>
2018-05-22 20:41:19 +0200

    juego terminado

---
+++ DarkWaves/Assets/ScoreManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+	public int score = 0;
+	private Text score_text;
+
+	void Awake(){
+		score_text = GetComponent<Text> ();
+	}
+
+	public void updateScore(int newScore){
+		score = newScore;
+		score_text.text = "Score: " + newScore;
+	}
+}
--- DarkWaves/Assets/attack_trigger.cs
+++ DarkWaves/Assets/attack_trigger.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class attack_trigger : MonoBehaviour {
 
+	public ScoreManager score_manager;
+
 	void OnTriggerEnter(Collider col){
 		if (col.CompareTag ("mummy") ) {
 			Debug.Log (col.gameObject.tag);
 			Debug.Log ("player: fuck u");
 			Destroy (col.gameObject);
+			score_manager.updateScore (score_manager.score + 1);
 		}
 	}
 }
